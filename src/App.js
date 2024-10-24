995b621876caee3cf3d1b2a8dba5a0b959e41f81
httpE2Barao <elias.barao@hotmail.com>
2024-10-24 15:23:24 -0300

    atualização do modelo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { Environment, OrbitControls, SoftShadows, useGLTF } from '@react-three/drei';
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import React, { Suspense } from 'react';
 import * as THREE from 'three';
 import './App.css';
@@ -13,17 +12,20 @@ function Model(props) {
       node.castShadow = true;
       node.receiveShadow = true;
 
-      // Personalizar o material do líquido
-      if (node.name.toLowerCase().includes('liquid') || node.material.name.toLowerCase().includes('water')) {
+      // Aplicar material apenas ao splash
+      if (node.name.toLowerCase() === 'splash') {
         node.material = new THREE.MeshPhysicalMaterial({
           color: new THREE.Color(0xADD8E6), // Azul claro e sutil (Light Blue)
-          roughness: 0.05,
+          roughness: 0.01, // Mantém o material suave, mas não totalmente reflexivo
           metalness: 0,
           clearcoat: 1,
-          clearcoatRoughness: 0.1,
-          transmission: 0.9, // Maior transparência
+          clearcoatRoughness: 0.0,
+          transmission: 0.99, // Transparência quase total
           ior: 1.33, // Índice de refração semelhante à água
-          reflectivity: 0.9, // Aumentar a reflexão para simular efeitos de ray tracing
+          reflectivity: 0.9, // Reflexão para simular efeitos realistas
+          thickness: 0.05, // Controla a espessura para obter uma sensação de volume
+          attenuationDistance: 2.0, // Aumenta a distância de atenuação para maior clareza
+          attenuationColor: new THREE.Color(0xADD8E6) // Leve tom azulado
         });
       }
     }
@@ -35,40 +37,31 @@ function Model(props) {
 function App() {
   return (
     <div className='App'>
-      <Canvas className='App-model' shadows camera={{ position: [5, 5, 5], fov: 45 }}>
-        <ambientLight intensity={0.3} />
-        <directionalLight
-          position={[10, 10, 5]}
-          intensity={1.5}
-          shadow-mapSize-width={2048}
-          shadow-mapSize-height={2048}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-        />
-        <pointLight position={[-10, 10, -10]} intensity={0.3} />
+      <Canvas
+        className='App-model'
+        shadows
+        gl={{ powerPreference: "high-performance", antialias: true }}
+        camera={{ position: [5, 5, 5], fov: 45 }}
+      >
         <Suspense fallback={null}>
-          <SoftShadows samples={30} radius={5} />
+          {/* Carregando o HDR que você baixou */}
+          <Environment files="/808-hdri-skies-com.hdr" background />
+          <ambientLight intensity={0.1} />
+          <directionalLight
+            position={[10, 10, 5]}
+            intensity={3}
+            castShadow
+            shadow-mapSize-width={4096}
+            shadow-mapSize-height={4096}
+            shadow-camera-far={50}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-10}
+          />
           <Model scale={[20, 20, 20]} position={[0, -1.5, 0]} />
-          {/* Utilizando um ambiente predefinido */}
-          <Environment preset="studio" background />
-
-          {/* Adicionar efeito de pós-processamento */}
-          <EffectComposer>
-            <Bloom
-              luminanceThreshold={0.3}
-              luminanceSmoothing={0.9}
-              intensity={0.06}
-            />
-          </EffectComposer>
         </Suspense>
         <OrbitControls />
-        <mesh receiveShadow position={[0, -2, 0]}>
-          <planeGeometry attach="geometry" args={[100, 100]} />
-          <shadowMaterial attach="material" opacity={0.5} />
-        </mesh>
       </Canvas>
       <footer className='App-footer'>
         Esse site foi feito por Elias Barão e Thiago Battista.
